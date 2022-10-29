11f0feb3709f406186d078be378f9734c9593660
PanTrakX <pantrakx@protonmail.com>
2022-10-29 19:29:19 +0300

    created a basic character model

--- core/models.py
+++ core/models.py
@@ -6,3 +6,12 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
+
+
+class Character(models.Model):
+
+    (user := models.ForeignKey(User, on_delete=models.CASCADE))
+    (name := models.CharField(unique=True, null=False, max_length=20))
+
+    def __str__(self) -> str:
+        return self.name
