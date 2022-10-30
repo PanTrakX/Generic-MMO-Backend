15b1e4b7f0ffc1bd2a30a8de355f2454426d8071
PanTrakX <pantrakx@protonmail.com>
2022-10-30 15:58:14 +0200

    improved the character viewset and serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,10 +1,15 @@
-from dataclasses import fields
 from rest_framework import serializers
 
 from core.models import Character
 
 
-class CharacterSerializer(serializers.ModelSerializer):
+class CharacterInputSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Character
+        fields = ['name']
+
+
+class CharacterOutputSerializer(serializers.ModelSerializer):
     class Meta:
         model = Character
         fields = '__all__'
--- api/viewsets.py
+++ api/viewsets.py
@@ -3,18 +3,38 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 from core.models import Character
-from api.serializers import CharacterSerializer
+from api.serializers import CharacterOutputSerializer, CharacterInputSerializer
 
 
 class CharactersViewSet(viewsets.ModelViewSet):
-
-    authentication_classes = [TokenAuthentication, ]
-    permission_classes = [IsAuthenticated, ]
+    """
+    Get: List/Retrieve all the owned character by the user from the request
+    Post: Create a character
+    """
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
 
     queryset = Character.objects.all()
-    serializer_class = CharacterSerializer
 
     def get_queryset(self):
+        """
+        Get only the instances that are owned by the user
+        """
         queryset = self.queryset
         query_set = queryset.filter(user=self.request.user)
         return query_set
+
+    def get_serializer_class(self):
+        """
+        Get different serializers based on the action input/output
+        """
+        if self.action == 'create' or self.action == 'update':
+            return CharacterInputSerializer
+        return CharacterOutputSerializer
+
+    def perform_create(self, serializer):
+        """
+        Fill the user field of the Character instance with the user from the request
+        """
+        serializer.save(user=self.request.user)
+        return super().perform_create(serializer)
