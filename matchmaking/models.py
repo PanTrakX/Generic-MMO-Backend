25880bd351374dbf08b47f2bd116cd89736c2bd7
PanTrakX <pantrakx@protonmail.com>
2022-11-01 20:24:20 +0200

    added current and max players and last_time_pinged fields to GameServerInstance

--- matchmaking/models.py
+++ matchmaking/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from django.db import models
 
 # Create your models here.
@@ -7,3 +8,6 @@ class GameServerInstance(models.Model):
 
     (ip := models.GenericIPAddressField())
     (port := models.PositiveIntegerField())
+    (current_players := models.PositiveIntegerField(default=0))
+    (max_players := models.PositiveIntegerField(default=0))
+    (last_time_pinged := models.TimeField())
