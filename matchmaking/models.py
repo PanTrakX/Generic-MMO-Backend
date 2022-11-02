fa1e1e1958915eadceaff0a56c11bd3a530ec39e
PanTrakX <pantrakx@protonmail.com>
2022-11-02 18:17:01 +0200

    added a default last_time_pinged and migrated

--- matchmaking/models.py
+++ matchmaking/models.py
@@ -1,6 +1,6 @@
-from email.policy import default
 from django.db import models
 
+from datetime import datetime
 # Create your models here.
 
 
@@ -10,4 +10,4 @@ class GameServerInstance(models.Model):
     (port := models.PositiveIntegerField())
     (current_players := models.PositiveIntegerField(default=0))
     (max_players := models.PositiveIntegerField(default=0))
-    (last_time_pinged := models.TimeField())
+    (last_time_pinged := models.DateTimeField(default=datetime.now))
